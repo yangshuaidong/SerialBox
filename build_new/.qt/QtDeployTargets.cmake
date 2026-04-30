78f5f027f0165b2f6f2a388d14da52f20c49c8e1
SerialBox Refactor <serialbox@refactor>
2026-04-30 23:09:06 +0800

    chore: 清理 build 产物，更新 .gitignore 排除 build_new/

--- build_new/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SerialBox_FILE E:/QT/SerialBox/build_new/SerialBox.exe)
-set(__QT_DEPLOY_TARGET_SerialBox_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SerialBox_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6SerialPort.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Network.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
--- build_new/CMakeFiles/SerialBox.dir/SerialBox_autogen/3YJK5W5UP7/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/SerialBox.dir/SerialBox_autogen/3YJK5W5UP7/qrc_resources.cpp.obj: \
- E:\QT\SerialBox\build_new\SerialBox_autogen\3YJK5W5UP7\qrc_resources.cpp
